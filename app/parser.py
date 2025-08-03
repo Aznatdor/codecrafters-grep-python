27dff74c71235a757d83f5fe79b72c8bd2464634
Aznatdor <abobrov333@gmail.com>
2025-08-03 15:42:59 +0300

    Add parser
    Now the code tries to find a match within an input string.
    To avoid complications with calculating true length of pattern
    (since "[abcde]" is actually a one symbol) the code tries to match
    pattern to input_line, input_line[1:], input_line[2:] and so on.
    
    Also fixed bugs in negative groups.

--- app/parser.py
+++ app/parser.py
@@ -1,10 +1,4 @@
-import sys
 import string
-import string
-
-# import pyparsing - available if you need it!
-# import lark - available if you need it!
-
 
 ALPHANUMERIC = set("_" + string.ascii_letters + string.digits)
 
@@ -30,19 +24,15 @@ def parse(pattern, input_string, pattern_ind=0, input_ind=0):
             if input_string[input_ind] in ALPHANUMERIC:
                 return parse(pattern, input_string, pattern_ind+1, input_ind+1)
             return False
-        elif pattern[pattern_ind] == "\\":
-            if pattern[pattern_ind] == input_string[input_ind]:
-                return parse(pattern, input_string, pattern_ind+1, input_ind+1)
-            return False
         else:
             raise Exception(f"Unknown metacharacter \\{pattern[pattern_ind]}")
 
     elif pattern[pattern_ind] == "[": # match a group
         # TO DO: invoke parse function after reacing "]". Otherwise it returns None
         # print("GROUP")
+        FLAG = True
         END = pattern.find("]", pattern_ind)
         if len(pattern) >= 2 and pattern[pattern_ind+1] == "^":
-            FLAG = True
             pattern_ind += 2
 
             while pattern_ind != END:
@@ -54,7 +44,8 @@ def parse(pattern, input_string, pattern_ind=0, input_ind=0):
                     subpattern = pattern[pattern_ind]
 
                 # try to match the current subpattern with the current character in the input
-                # if at least one match, we should return False
+                print(subpattern, input_string[input_ind])
+                # if at least 1 match, return False
                 if parse(subpattern, input_string[input_ind]):
                     FLAG = False
                     break
@@ -67,7 +58,6 @@ def parse(pattern, input_string, pattern_ind=0, input_ind=0):
 
         else:
             pattern_ind += 1
-            FLAG = False
 
             while pattern_ind != END:
                 if pattern[pattern_ind] == "\\":
@@ -91,30 +81,8 @@ def parse(pattern, input_string, pattern_ind=0, input_ind=0):
         return False
 
 
-def main():
-    pattern = sys.argv[2]
-    input_line = sys.stdin.read()
-
-
-    if sys.argv[1] != "-E":
-        print("Expected first argument to be '-E'")
-        exit(1)
-
-
-    pattern_len = len(pattern) - pattern.count("\\")
-    input_len = len(input_line)
-
-
-    for start_pos in range(input_len):
-        if parse(pattern, input_line, input_ind=start_pos):
-            # print(True)
-            exit(0)
-
-    exit(1)
-    return 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-
 if __name__ == "__main__":
-    main()
+    print(parse(r"[^ban]", "b"))
+    print(parse(r"[^ban]", "a"))
+    print(parse(r"[^ban]", "n"))
+    print(parse(r"[^ban]", "i"))
