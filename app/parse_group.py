7e3d095bdf54a654d0a61aa65717adeffbffa0f9
Aznatdor <abobrov333@gmail.com>
2025-08-21 16:16:47 +0300

    Proper parser for () groups

---
+++ app/parse_group.py
@@ -0,0 +1,45 @@
+# Algortihm for parsing groups
+
+def parse_group(pattern):
+    """
+        args:
+            pattern: str - some string with groups (arbitrary depth) encosed by "(", ")" and splitted with "|"
+                           without loss of generality pattern should begin with ( and end with )
+            
+        returns:
+            groups: list - list, where each element is group
+    """
+
+    stack = [[]]
+    curr = ""
+
+    for char in pattern:
+        if char == "(":
+            stack.append([])
+        elif char == ")":
+            if curr:
+                stack[-1].append(curr)
+            curr = ""
+            tmp = stack.pop()
+            stack[-1].append(tmp)
+        elif char == "|":
+            if curr:
+                stack[-1].append(curr)
+            curr = ""
+        else:
+            curr += char
+
+    if len(stack) > 1:
+        raise(ValueError("Unmatched parentheses"))
+
+    return stack[0]
+
+
+
+if __name__ == "__main__":
+    #pattern = "((abc|bca)|a|b|((a|b)|(b|c)))(abc|(a|b))()"
+    pattern = "(abc)"
+    print(pattern)
+    res = parse_group(pattern)
+    for group in res:
+        print(group)
